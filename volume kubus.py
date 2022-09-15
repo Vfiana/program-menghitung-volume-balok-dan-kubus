cfe3f14470bc6774b5164d91f6f00c03bb0c7d9a
Vfiana <113625769+Vfiana@users.noreply.github.com>
2022-09-15 23:59:39 +0700

    Add files via upload

---
+++ volume kubus.py
@@ -0,0 +1,13 @@
+# Membuat Program Menghitung Volume Kubus
+
+sisi = 5
+
+vol = sisi*sisi*sisi
+print(vol)
+
+# Membuat Program Menghitung Volume Kubus
+
+S = int(input("Masukkan Angka Sisi"))
+
+Volume = S*S*S
+print(Volume)
