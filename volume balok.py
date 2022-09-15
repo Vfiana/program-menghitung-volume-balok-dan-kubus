31e895f403a552e1ff1ce900189bacb61330aacf
Vfiana <113625769+Vfiana@users.noreply.github.com>
2022-09-15 23:58:58 +0700

    Add files via upload

---
+++ volume balok.py
@@ -0,0 +1,18 @@
+# Membuat Progam Menghitung Bangun Ruang Balok
+
+panjang = 30
+lebar = 10
+tinggi = 15
+
+volume = Panjang*lebar*tinggi
+print(volume)
+
+# Membuat Program Menghitung Bangun Ruang Balok
+
+p = int(input("Masukkan Angka : "))
+l = int(input("Masukkan Angka : "))
+t = int(input("Masukkan Angka : "))
+
+v = p*l*t 
+print(v)
+
